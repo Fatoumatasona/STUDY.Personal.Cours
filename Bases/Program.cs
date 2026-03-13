4fc3fa1b03edeb483df0a05090f063f39086a7ac
sonas <sonasylla26@gmail.com>
2026-03-13 09:45:13 +0100

    Cours sur l'interprétation des chaînes

--- Bases/Program.cs
+++ Bases/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, C# Academy !");
+﻿Console.WriteLine("Bonjour ! Comment t'appeles-tu ?");
+string? rep = Console.ReadLine();
+
+Console.WriteLine("Bonjour " + rep + " !");
+
+Console.WriteLine("Quelle est ton année de naissance ?");
+rep = Console.ReadLine();
+
+int annéeNais = int.Parse(rep);
+
+int age = DateTime.Today.Year - annéeNais;
+Console.WriteLine($"Tu as ou vas avoir {age} ans en {DateTime.Today.Year}");
+
