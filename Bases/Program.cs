d1f6f89f5c1dead8caffcd9ba4d4ab8a70c72608
sonas <sonasylla26@gmail.com>
2026-03-13 10:09:11 +0100

    Instruction if et else et l'utilisation de l'opérateur ternaire (3 expressions utilisées)

--- Bases/Program.cs
+++ Bases/Program.cs
@@ -8,6 +8,34 @@ rep = Console.ReadLine();
 
 int annéeNais = int.Parse(rep);
 
-int age = DateTime.Today.Year - annéeNais;
-Console.WriteLine($"Tu as ou vas avoir {age} ans en {DateTime.Today.Year}");
+const int annéeMin = 1900;
 
+if (annéeNais >= annéeMin && annéeNais <= DateTime.Today.Year)
+{
+
+    int age = DateTime.Today.Year - annéeNais;
+    Console.WriteLine($"Tu as ou vas avoir {age} ans en {DateTime.Today.Year}");
+
+    string tranche = string.Empty;
+    if (age <= 25)
+        tranche = "Tu as moins de 25 ans";
+    else if (age >= 25 && age < 40)
+        tranche = "25 - 40 ans ";
+    else if (age >= 40 && age < 55)
+        tranche = "40 - 55 ans ";
+    else
+        tranche = "plus de 55 ans ";
+
+    Console.WriteLine($"Ta tranche d'âge est : {tranche}");
+}
+else
+{
+    Console.WriteLine($"Ton année de naissance doit être comprise entre {annéeMin} et {DateTime.Today.Year}");
+}
+
+Console.WriteLine("As tu un code de reduction (O/N)");
+rep = Console.ReadLine();
+
+bool reduc = (rep == "O");
+
+Console.WriteLine(annéeNais < 2000 ? "Tu es né au 20ème siècle" : "Tu es née au 21 ème siècle");
