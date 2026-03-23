2f057ae8adcc8ed17115bbbe9a7dd92ac616a60e
sonas <sonasylla26@gmail.com>
2026-03-23 10:47:00 +0100

    Les paramètres d'entrée, Modification des classes OutilsTexte et OutilsMaths

--- Fonctions/OutilsMaths.cs
+++ Fonctions/OutilsMaths.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace Fonctions
 {
+
+    /// <summary>
+	/// Fournit des fonctions mathématiques supplémentaires par rapport à celles de .Net
+	/// </summary>
     internal class OutilsMaths
     {
+        public static double GetValeurMaxi(params double[] nombres)
+        {
+            double max = double.MinValue;
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                if (nombres[i] > max) max = nombres[i];
+            }
+
+            return max;
+        }
     }
 }
--- Fonctions/Program.cs
+++ Fonctions/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             int nbMots = OutilsTexte.CompterMots("j'aime le C#");
+
+            //double[] tabNombres = { 4.21, 5.54, 2.13, 3.47 };
+            double max1 = OutilsMaths.GetValeurMaxi(4.21, 5.54, 2.13, 3.47, 4.58, 69);
+            Console.WriteLine("Valeur maxi : " + max1);
+
+            string traduction = OutilsTexte.Traduire("J'aime le C#");
+
         }
     }
 }
